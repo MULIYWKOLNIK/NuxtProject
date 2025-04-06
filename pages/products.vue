0759aadedb9c968910f6c8a2d29d8e5ad9ede054
MULIYWKOLNIK <fesuksergij399@gmail.com>
2025-04-06 17:02:15 +0300

    docs: додано нові модулі feat: додано надпис

---
+++ pages/products.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Список продуктів</h1>
+    <!-- Контент сторінки для продуктів -->
+  </div>
+    <div>
+      <h1>Список продуктів</h1>
+      <ul>
+        <li>Продукт 1</li>
+        <li>Продукт 2</li>
+        <li>Продукт 3</li>
+      </ul>
+    </div>
+
+
+</template>
+
+<script setup>
+definePageMeta({
+  title: 'Список продуктів'
+})
+</script>
+
+<style scoped>
+/* Стилі для сторінки */
+</style>
